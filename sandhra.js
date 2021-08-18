7d88913d6c1da8f1e83016faab6b848c5aa913fa
sandhrarose <89066343+sandhrarose@users.noreply.github.com>
2021-08-18 09:04:59 +0530

    Add files via upload

---
+++ sandhra.js
@@ -0,0 +1,12 @@
+function makeRed(){
+    document.body.style.backgroundColor="red";
+}
+function makeViolet(){
+    document.body.style.backgroundColor="violet";
+}
+function makeGreen(){
+    document.body.style.backgroundColor="green";
+}
+function makeOrange(){
+    document.body.style.backgroundColor="orange";
+}
